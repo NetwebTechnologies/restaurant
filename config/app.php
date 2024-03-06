0c8b53f919127c803498705eff3cc78e0036dbac
jagjot <jagjot.netweb@gmail.com>
2024-03-06 15:49:21 +0530

    serviceProvider

--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Collective\Html\HtmlServiceProvider::class,
-        Netweb\Restaurant\RestaurantServiceProvider::class,
+        \Netweb\Restaurant\RestaurantServiceProvider::class
 
     ],
 
