e8ad9d598c41a2a17f62b6777f9610e9810f7204
jagjot <jagjot.netweb@gmail.com>
2024-03-06 14:29:23 +0530

    old

--- NetwebPackage/Restaurant/src/RestaurantServiceProvider.php
+++ NetwebPackage/Restaurant/src/RestaurantServiceProvider.php
@@ -7,7 +7,6 @@ use Settings;
 
 class RestaurantServiceProvider extends ServiceProvider {
     public function boot() {
-        // dd(__DIR__.'/config/restaurant.php');
         // Load Routes
         $this->loadRoutesFrom(__DIR__ . '/routes/nwt-restaurant.php');
 
@@ -19,21 +18,22 @@ class RestaurantServiceProvider extends ServiceProvider {
 
         // Publish Config
         $this->publishes([
-            __DIR__.'\config\restaurant.php' => config_path('restaurant.php')
+            __DIR__.'/config/restaurant.php' => config_path('restaurant.php')
         ], 'nwt-restaurant');
 
         // $this->publishes([
         //    // Publish views
-        //    __DIR__.'/vendor/nwt-restaurant' => public_path('vendor/nwt-restaurant'),
+        //     __DIR__.'/views/assets/' => public_path('vendor/nwt-restaurant'),
         // ], 'nwt-restaurant');
 
-        // // Publish Migrations
-        // $this->publishes([
-        //     __DIR__ . '/database/migrations/2024_02_21_113013_create_restaurant_names_table.php' =>
-        //     database_path('migrations/2024_02_21_113013_create_restaurant_names_table.php'),
-        //     __DIR__ . '/database/migrations/2024_02_27_054842_create_restaurant_types_table.php' =>
-        //     database_path('migrations/2024_02_27_054842_create_restaurant_types_table.php')
-        // ], 'nwt-restaurant');
+        // Publish Migrations
+        $this->publishes([
+            __DIR__ . '/database/migrations/2024_02_21_113013_create_restaurant_names_table.php' =>
+            database_path('/migrations/2024_02_21_113013_create_restaurant_names_table.php'),
+            __DIR__ . '/database/migrations/2024_02_27_054842_create_restaurant_types_table.php' =>
+            database_path('/migrations/2024_02_27_054842_create_restaurant_types_table.php')
+        ], 'nwt-restaurant');
+
 
     }
 
