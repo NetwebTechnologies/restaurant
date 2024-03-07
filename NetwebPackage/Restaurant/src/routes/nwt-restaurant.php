9be43ea579df95dc95b787c0e77bc0c7c81490f6
jagjot <jagjot.netweb@gmail.com>
2024-03-07 15:31:10 +0530

    both routes inside web | auth middleware

--- NetwebPackage/Restaurant/src/routes/nwt-restaurant.php
+++ NetwebPackage/Restaurant/src/routes/nwt-restaurant.php
@@ -4,11 +4,13 @@ use Illuminate\Support\Facades\Route;
 use Netweb\Restaurant\Http\Controllers\RestaurantNameController;
 use Netweb\Restaurant\Http\Controllers\RestaurantTypeController;
 
-Route::resource('restaurant-name', RestaurantNameController::class);
 
 
 Route::group(['middleware' => 'web'], function() {
     Route::group(['middleware' => 'auth'], function () {
+
+        Route::resource('restaurant-name', RestaurantNameController::class);
         Route::resource('restaurant_types', RestaurantTypeController::class);
+
     });
 });
